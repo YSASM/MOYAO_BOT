d8af59f77100ccd76542b9270b26eea9b3cf2c9d
YSASM <80308986+YSASM@users.noreply.github.com>
2022-09-28 21:09:45 +0800

    Update BotServer.py

--- service/BotServer.py
+++ service/BotServer.py
@@ -1,7 +1,7 @@
 from mirai import Mirai, WebSocketAdapter
 def bot_config():
     bot = Mirai(
-    qq=2819207569, 
+    qq=【qq】, 
     adapter=WebSocketAdapter(
         verify_key='12345678', host='localhost', port=8008
         )
